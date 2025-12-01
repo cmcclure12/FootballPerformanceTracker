2b098f52cb6f9b10dc8f79c4f1b961f4f61d0050
Connor Mc Clure <ST10435598@vcconnect.edu.za>
2025-12-01 18:55:54 +0200

    Add project files.

---
+++ FootballPerformanceTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FootballPerformanceTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
