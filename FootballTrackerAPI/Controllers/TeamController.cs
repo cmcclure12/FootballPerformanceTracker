7fd0892647a51481d624e972b02fd8ba059f3013
Connor Mc Clure <ST10435598@vcconnect.edu.za>
2025-12-09 17:55:16 +0200

    updates

---
+++ FootballTrackerAPI/Controllers/TeamController.cs
@@ -0,0 +1,41 @@
+﻿using FootballTrackerData.Data;
+using FootballTrackerData.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballTrackerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamController : ControllerBase
+    {
+        private readonly FootballDbContext _context;
+        public TeamController(FootballDbContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Team>>> GetTeams() 
+        {
+            var teams = await _context.Team.OrderByDescending(t => t.TeamName).ToListAsync();
+            return Ok(teams);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSelectedTeam(int id) 
+        {
+            var selectedTeam = _context.Team.FindAsync(id);
+            if (selectedTeam == null) 
+            {
+                return NotFound();
+            }
+            return Ok(selectedTeam);
+        }
+
+
+
+
+    }
+}
